5c4559b1f5fc6cbc6f94fcae0a62d96fed36c863
Alexander Dalinger <alexander.dalinger@qaware.de>
2023-06-14 10:30:53 +0200

    Fix test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -4,6 +4,6 @@ import App from '../main/App';
 
 test('renders learn react link', () => {
     render(<App/>);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/Enzymeinheiten pro 1g Fett/i);
     expect(linkElement).toBeInTheDocument();
 });
